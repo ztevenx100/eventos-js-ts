d4817a2bb7398d9cdacd7da773eb29953ec7da4d
Steven Castiblanco <50757337+ztevenx100@users.noreply.github.com>
2024-12-03 22:00:14 -0500

    TASK: Reto de programacion dia 2

--- js-adventjs-2024/reto-01/main.js
+++ js-adventjs-2024/reto-01/main.js
@@ -9,3 +9,16 @@
 function prepareGifts(gifts) {
     return [...new Set(gifts)].sort((a,b) => a-b);
 }
+
+const gifts1 = [3, 1, 2, 3, 4, 2, 5]
+const preparedGifts1 = prepareGifts(gifts1)
+console.log(preparedGifts1) // [1, 2, 3, 4, 5]
+
+const gifts2 = [6, 5, 5, 5, 5]
+const preparedGifts2 = prepareGifts(gifts2)
+console.log(preparedGifts2) // [5, 6]
+
+const gifts3 = []
+const preparedGifts3 = prepareGifts(gifts3)
+console.log(preparedGifts3) // []
+// No hay regalos, la lista queda vacía
