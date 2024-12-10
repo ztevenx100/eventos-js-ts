7548377d313ddf669668604f4fd1ecf18beefda2
Steven Castiblanco <50757337+ztevenx100@users.noreply.github.com>
2024-12-09 23:52:51 -0500

    TASK: Reto de programacion dia 7

---
+++ js-adventjs-2024/reto-07/main.js
@@ -0,0 +1,39 @@
+/* Reto #7: 👹 El ataque del Grinch */
+/** 
+ * ¡El grinch 👹 ha pasado por el taller de Santa Claus! Y menudo desastre ha montado. Ha cambiado el orden de algunos paquetes, por lo que los envíos no se pueden realizar.
+ * Por suerte, el elfo Pheralb ha detectado el patrón que ha seguido el grinch para desordenarlos. Nos ha escrito las reglas que debemos seguir para reordenar los paquetes. Las instrucciones que siguen son:
+ * Recibirás un string que contiene letras y paréntesis.
+ * Cada vez que encuentres un par de paréntesis, debes voltear el contenido dentro de ellos.
+ * Si hay paréntesis anidados, resuelve primero los más internos.
+ * Devuelve el string resultante con los paréntesis eliminados, pero con el contenido volteado correctamente.
+ * Nos ha dejado algunos ejemplos:
+
+fixPackages('a(cb)de')
+// ➞ "abcde"
+// Volteamos "cb" dentro de los paréntesis
+
+fixPackages('a(bc(def)g)h')
+// ➞ "agdefcbh"
+// 1º volteamos "def" → "fed", luego volteamos "bcfedg" → "gdefcb"
+
+fixPackages('abc(def(gh)i)jk')
+// ➞ "abcighfedjk"
+// 1º volteamos "gh" → "hg", luego "defhgi" → "ighfed"
+
+fixPackages('a(b(c))e')
+// ➞ "acbe"
+// 1º volteamos "c" → "c", luego "bc" → "cb"
+ */
+
+/** @param {string} packages with parentheses
+ *  @returns {string} Fixed and sorted packages
+ */
+function fixPackages(packages) {
+  while (packages.includes('(')) {
+    packages = packages.replace(/\(([^()]+)\)/g, (match, group) => {
+      // Volteamos el contenido dentro del grupo
+      return group.split('').reverse().join('');
+    });
+  }
+  return packages;
+}
